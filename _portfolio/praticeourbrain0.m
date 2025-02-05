3f789d7eb759c63ce63ad428985b57e4c441c640
Aijunly <162404485+Aijunly@users.noreply.github.com>
2025-02-05 12:07:30 +0800

    Create praticeourbrain0.m

---
+++ _portfolio/praticeourbrain0.m
@@ -0,0 +1,7 @@
+---
+title: "Our Learning, Studying and Researching likes Machine Learning, Studying and Researching from Humanbeing"
+excerpt: "Please help our brain with MATHEMATICA <br/><img src='/images/500x300.png'>"
+collection: portfolio  
+---
+
+This is an item in your portfolio. It can be have images or nice text. If you name the file .md, it will be parsed as markdown. If you name the file .html, it will be parsed as HTML. 
