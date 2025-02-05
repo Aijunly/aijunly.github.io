d669d6f83acdd96f37ecb380d612ac411f449669
Aijunly <162404485+Aijunly@users.noreply.github.com>
2025-02-05 12:10:47 +0800

    Update and rename praticeourbrain0.m to 00HelpBrain.m

--- _portfolio/00HelpBrain.m
+++ _portfolio/00HelpBrain.m
@@ -1,7 +1,26 @@
 ---
 title: "Our Learning, Studying and Researching likes Machine Learning, Studying and Researching from Humanbeing"
 excerpt: "Please help our brain with MATHEMATICA <br/><img src='/images/goals.PNG'>"
-collection: portfolio  
+collection: portfolio                      
 ---
 
 This is an item in your portfolio. It can be have images or nice text. If you name the file .md, it will be parsed as markdown. If you name the file .html, it will be parsed as HTML. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
